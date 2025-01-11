510e61fea2f767631ba5f59b2c8b4461814f583f
Biswas-Souvik <biswassouvik284@gmail.com>
2025-01-11 12:20:03 +0530

    Added Paginations

--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializer, CategorySerializer
 from rest_framework import generics
 
 
 # Create your views here.
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    ordering_fields = ['price', 'inventory']
+    filterset_fields = ['price', 'inventory']
+    search_fields = ['category']
 
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
