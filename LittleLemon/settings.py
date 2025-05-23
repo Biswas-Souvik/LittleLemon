816c3c624a9fb20f5dde19edff73b58e26952fee
Biswas-Souvik <biswassouvik284@gmail.com>
2025-05-23 21:05:34 +0530

    Added Images, css and templates

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'LittleLemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Restaurant/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Restaurant/static/'
+
+STATICFILES_DIRS = [
+    "Restaurant/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
