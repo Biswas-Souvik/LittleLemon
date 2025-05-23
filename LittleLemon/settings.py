b02fba1c13c2368f99de12c647d314328570b242
Biswas-Souvik <biswassouvik284@gmail.com>
2025-05-23 20:51:49 +0530

    Added Booking and Menu Model

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'LittleLemonDRF.apps.LittlelemondrfConfig',
+    'Restaurant.apps.RestaurantConfig',
 ]
 
 MIDDLEWARE = [
--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Restaurant.urls')),
     path('api/', include('LittleLemonDRF.urls')),
 ]
