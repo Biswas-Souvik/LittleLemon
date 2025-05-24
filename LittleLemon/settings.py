8dc55b4b38297c943d364f07c034e82bea00b940
Biswas-Souvik <biswassouvik284@gmail.com>
2025-05-24 12:56:17 +0530

    Fixed small bugs

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'Restaurant/static/'
 
 STATICFILES_DIRS = [
     "Restaurant/static",
